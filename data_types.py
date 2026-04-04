146df4609edaf40ad9bd0b1fa49afd06e68d4e83
Anushka Singh <ms.anushka2108@gmail.com>
2026-04-04 20:14:19 +0530

    Data Types in Python

---
+++ data_types.py
@@ -0,0 +1,44 @@
+'''Python data types include:-
+   1. Numeric - int (integer), float (floating point numbers), complex numbers (it has 2
+                parts: real and imaginary)
+   2. Boolean - bool (it has only 2 values- True or False)
+   3. Dictionary
+   4. Set
+   5. Sequence - list, tuple, str(string), range (also a function used in loops)
+   6. None''' 
+#Below are different data types used in Python along with examples.
+
+#string data type
+name = "Anushka"
+print(name)    #Output: Anushka
+
+#printing a single character
+letter = 'A'
+print(letter)    #Output: A
+
+#numbers
+#printing an integer
+num1 = 98
+print(num1)    #Output: 98
+
+#printing a floating point number
+num2 = 98.8
+num3 = 96.6
+print(num2,num3)    #Output: 98.8 96.6
+
+#printing a complex number
+num4 = 6+10j
+num5 = 5-2j
+print(num4, num5)    #Output: (6+10j) (5-2j)
+
+#boolean data type
+rate = True
+invest = False
+print(rate)        #Output: True
+print(invest)      #Output: False
+
+#none data type
+rate = None
+invest = None
+print(rate)        #Output: None
+print(invest)      #Output: None
