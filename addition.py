6688c22302e26975ffbf3ce1428c8ba33c654798
Anushka Singh <ms.anushka2108@gmail.com>
2026-04-04 20:18:42 +0530

    Addition of int, float, and Complex Numbers

---
+++ addition.py
@@ -0,0 +1,21 @@
+#performing various arithmetic operations in Python
+
+#addition
+
+#integers
+num1 = 10
+num2 = 15
+sum1 = num1+num2
+print(sum1)    #Output: 25
+
+#floating point numbers
+num3 = 10.1
+num4 = 16.2
+sum2 = num3+num4
+print(sum2)    #Output: 26.299999999999997
+
+#complex numbers
+num5 = 6+10j
+num6 = 5-2j
+sum3 = num5+num6
+print(sum3)    #Output: (11+8j)
