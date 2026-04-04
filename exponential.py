c5ad48e4e067faf04b714d3f6d0fa7a962e3d273
Anushka Singh <ms.anushka2108@gmail.com>
2026-04-04 20:29:50 +0530

    Exponen (Power of) Operations

---
+++ exponential.py
@@ -0,0 +1,19 @@
+#exponents
+
+#integers
+num1 = 10 
+num2 = 2
+expo1 = num1**num2
+print(expo1)    #Output: 100
+
+#floating point numbers
+num3 = 5.1
+num4 = 2.2
+expo2 = num3**num4
+print(expo2)    #Output: 36.029180782470974
+
+#complex numbers
+num5 = 2+2j
+num6 = 1-2j
+expo3 = num5**num6
+print(expo3)    #Output: (3.7176395160348648-13.088342045157981j)
