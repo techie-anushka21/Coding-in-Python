bedd768334b44fe2993cddd89c8fc18edc96247a
Anushka Singh <ms.anushka2108@gmail.com>
2026-04-04 20:22:14 +0530

    Multiplication of int, float, and Complex Numbers

---
+++ multiplication.py
@@ -0,0 +1,19 @@
+#multiplication
+
+#integers
+num1 = 10
+num2 = 5
+pro1 = num1*num2
+print(pro1)    #Output: 50
+
+#floating point numbers
+num3 = 10.1
+num4 = 16.2
+pro2 = num3*num4
+print(pro2)    #Output: 163.61999999999998
+
+#complex numbers
+num5 = 6+10j
+num6 = 5-2j
+pro3 = num5*num6
+print(pro3)    #Output: (50+38j)
