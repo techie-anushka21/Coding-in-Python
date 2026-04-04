8dbcbae2df3342bdfbcb07e3444f915e62b9358e
Anushka Singh <ms.anushka2108@gmail.com>
2026-04-04 20:27:07 +0530

    Division of all 3 types

---
+++ division2.py
@@ -0,0 +1,11 @@
+#dividing 10 by 3 to get remainder, half and full quotient.
+num1 = 10    #dividend 
+num2 = 3    #divisor
+rem = num1%num2  #remainder
+hqt = num1//num2 #half quotient
+fqt = num1/num2  #full quotient
+print("1st no.: ",num1)
+print("2nd no.: ",num2)
+print("Remainder = ", rem)
+print("Half quotient = ", hqt)
+print("Full quotient = ", fqt)
