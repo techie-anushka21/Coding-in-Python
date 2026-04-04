5db42e7b46942adfae786ce548ceaeb17b6b6f58
Anushka Singh <ms.anushka2108@gmail.com>
2026-04-04 20:25:57 +0530

    Division (half quotient, full quotient, remainder) of int, float, and Complex Numbers

---
+++ division.py
@@ -0,0 +1,51 @@
+#half quotient
+
+#integers
+num1 = 5
+num2 = 2
+hq1 = num1//num2
+print(hq1)    #Output: 2
+
+#floating point numbers
+num3 = 5.5
+num4 = 2.5
+hq2 = num3//num4
+print(hq2)    #Output: 2.0
+
+#complex numbers = not supported by half quotient
+
+#full quotient
+
+#integers
+num5 = 5
+num6 = 2
+hq3 = num5/num6
+print(hq3)    #Output: 2.5
+
+#floating point numbers
+num7 = 5.5
+num8 = 2.5
+hq4 = num7/num8
+print(hq4)    #Output: 2.2
+
+#complex numbers
+num9 = 6+10j
+num10 = 5-2j
+hq5 = num9/num10
+print(hq5)    #Output: (0.3448275862068966+2.137931034482759j)
+
+#remainder
+
+#integers
+num11 = 5
+num12 = 2
+rem1 = num11%num12
+print(rem1)    #Output: 1
+
+#floating point numbers
+num13 = 5.5
+num14 = 2.5
+rem2 = num13/num14
+print(rem2)    #Output: 2.2
+
+#complex numbers = not supported by remainder
