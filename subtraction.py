78cab7a559e8dcc23e7ad770018c993f56fdc96c
Anushka Singh <ms.anushka2108@gmail.com>
2026-04-04 20:20:18 +0530

    Subtraction of int, float, and Complex Numbers

---
+++ subtraction.py
@@ -0,0 +1,19 @@
+#performing subtraction
+
+#integers
+num1 = 15
+num2 = 10
+diff1 = num1 - num2
+print(diff1)    #Output: 5
+
+#floating point numbers
+num3 = 16.2
+num4 = 10.1
+diff2 = num3 - num4
+print(diff2)    #Output: 6.1
+
+#complex numbers
+num5 = 6+10j
+num6 = 5-2j
+diff3 = num5 - num6
+print(diff3)    #Output: (1+12j)
