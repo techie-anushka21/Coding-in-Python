156fca5e463f3692e788ab1a41def8bc61797c6c
Anushka Singh <ms.anushka2108@gmail.com>
2026-04-05 14:15:22 +0530

    Sum and Average of 3 nos. taken from the User

---
+++ sum_avg2.py
@@ -0,0 +1,21 @@
+#read 3 numbers and find their sum and average.
+
+num1 = int(input("Enter the 1st no.: "))
+num2 = int(input("Enter the 2nd no.: "))
+num3 = int(input("Enter the 3rd no.: "))
+print("Numbers entered:",num1,num2,num3)
+sum = num1+num2+num3
+avg = sum/3
+print("Sum =",sum)
+print("Average =",avg)
+
+'''
+Output:-
+
+Enter the 1st no.: 9    #Entered by User
+Enter the 2nd no.: 2    #Entered by User
+Enter the 3rd no.: 5    #Entered by User
+Numbers entered: 9 2 5
+Sum = 16
+Average = 5.333333333333333
+'''
