fee3b3b5f468175c136f1c253e01ec86a361f881
Anushka Singh <ms.anushka2108@gmail.com>
2026-04-05 14:12:32 +0530

    Sum and Average of 5 nos.

---
+++ sum_avg.py
@@ -0,0 +1,19 @@
+#find sum and average of 5, 10.6, 7, 12, 3. print both with proper prompt messages.
+num1 = 5
+num2 = 10.6
+num3 = 7
+num4 = 12
+num5 = 3
+print("Given numbers: ",num1,", ",num2,", ",num3,", ",num4,", ",num5) #printing the 5 values
+sum = num1 +num2 + num3 + num4 + num5    #calculating the sum
+avg = sum/5    #calculating the average
+print("Sum of all the above numbers = ", sum)    #printing the sum with proper prompt message
+print("Average of all the above numbers = ",avg)    #printing the sum with proper prompt message
+
+'''
+Output:-
+
+Given numbers:  5 ,  10.6 ,  7 ,  12 ,  3
+Sum of all the above numbers =  37.6
+Average of all the above numbers =  7.5200000000000005
+'''
