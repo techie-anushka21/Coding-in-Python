bbbd747b639f59fb7ed86279a3cae7df8fd6a149
Anushka Singh <ms.anushka2108@gmail.com>
2026-04-05 14:27:41 +0530

    Taking radius from the User and calculating Area and Circumference.

---
+++ circle_radius.py
@@ -0,0 +1,18 @@
+#read radius of a circle then calculate area and circumference.
+
+PI = 3.14
+r = float(input("Enter radius: "))
+print("Radius of the circle =",r)
+area = PI*r**2
+circum = 2*PI*r
+print("Area =",area)
+print("Cirumference =",circum)
+
+'''
+Output:-
+
+Enter radius: 3.5
+Radius of the circle = 3.5
+Area = 38.465
+Cirumference = 21.98
+'''
