460462dd31b0d53764da7b3e2760d425a162809c
Anushka Singh <ms.anushka2108@gmail.com>
2026-04-05 14:08:26 +0530

    Swapping of 2 numbers (integers)

---
+++ swapping.py
@@ -0,0 +1,21 @@
+#swapping of 2 numbers (12 and 7)
+num1 = 12
+num2 = 7
+print("Before swapping:-")
+print("num1 = ",num1)
+print("num2 = ",num2)
+temp = num1    #swapping starts
+num1 = num2  
+num2 = temp    #swapping ends
+print("After swapping:-")
+print("num1 = ",num1)
+print("num2 = ",num2)
+
+'''
+Before swapping:-
+num1 =  12
+num2 =  7
+After swapping:-
+num1 =  7
+num2 =  12
+'''
