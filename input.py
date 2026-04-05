bb4cec02d3f9e0be16343e2a3ee495d53614c024
Anushka Singh <ms.anushka2108@gmail.com>
2026-04-05 14:18:48 +0530

    Using input() function to take values from the User.

---
+++ input.py
@@ -0,0 +1,18 @@
+#input in Python- str, int, float
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+cgpa = float(input("Enter your cgpa: "))
+print("Name:",name)
+print("Age:",age)
+print("CGPA:",cgpa)
+
+'''
+Output:-
+
+Enter your name: Anita   #Entered by the User
+Enter your age: 18       #Entered by the User
+Enter your cgpa: 9.2     #Entered by the User
+Name: Anita
+Age: 18
+CGPA: 9.2
+'''
