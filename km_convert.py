9a68c122af0d232b7c63a7ee1a1c717314aab4f4
Anushka Singh <ms.anushka2108@gmail.com>
2026-04-05 14:33:20 +0530

    Converting km to m.

---
+++ km_convert.py
@@ -0,0 +1,15 @@
+#convert km to m and cm.
+
+dist = float(input("Enter distance in km: "))
+m = dist*1000
+cm = dist*100000
+print(dist,"km in m =",m)
+print(dist,"km in cm =",cm)
+
+'''
+Output:-
+
+Enter distance in km: 15.7
+15.7 km in m = 15700.0
+15.7 km in cm = 1570000.0
+'''
