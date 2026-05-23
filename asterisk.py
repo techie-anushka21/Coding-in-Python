9def875b92d41ed1107ff1b37d50f0a7bb7a7e17
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-23 20:28:27 +0530

    String Manipulation Operator- Asterisk

---
+++ asterisk.py
@@ -0,0 +1,27 @@
+#multiplying/repeating a strings with asterisk operator (*)- A string manipulation operator
+'''
+- It multiplies a string as many times as required. 
+-SYNTAX:-
+ var_name = "string-1" * no._of_times
+'''
+#Example-1
+a = "Eisha "
+b = a*3
+print(b)
+
+'''
+Output: -
+
+Eisha Eisha Eisha 
+'''
+
+#Example-2
+l = "Happy "
+q = l*4
+print(q)
+
+'''
+Output: -
+
+Happy Happy Happy Happy 
+'''
