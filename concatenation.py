0a4cdf0fc00f77ecff41791fed6ae56dd22aa0cc
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-23 20:18:31 +0530

    String Manipulation Operator - Concatenation

---
+++ concatenation.py
@@ -0,0 +1,32 @@
+#concatenating 2 strings with concatenation operator (+)- A string manipulation operator
+'''
+- It concatenates 2 or more strings to make them as one. 
+-SYNTAX:-
+ var1_name = "string-1"
+ var2_name = "string-2"
+ var3_name = var1_name + var_name2
+'''
+
+#example 1:-
+fname = "Eisha "
+lname = "Kumari"
+full_name = fname+lname
+print(full_name)
+
+'''
+Output: -
+
+Eisha Kumari
+'''
+
+#example 2:-
+a = "Hello "
+b = "World!"
+c = a+b
+print(c)
+
+'''
+Output: -
+
+Hello World!
+'''
