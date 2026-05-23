c0f9c30c92f7bdbb612a5928f7d336544d9a5041
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-23 21:48:30 +0530

    Various String functions in Python.

---
+++ string_functions.py
@@ -0,0 +1,14 @@
+'''
+There are various functions for strings in Python: -
+
+1. len() : it determines length of a string.
+2. split() : it separates the substrings of a string with the given separator character (space or ,).
+3. count() : it counts the no. of total substrings in a string.
+4. replace() : it replaces a substring with another substring in a string.
+5. find() : it finds a substring from the given string.
+6. upper() : it converts the string to uppercase.
+7. lower() : it converts the string to lowercase.
+8. title() : it converts the 1st character of every substring to UPPERCASE.
+9. swapcase() : it converts lowercase string to UPPERCASE and vice versa.
+10. format() : it formats the string.
+'''
