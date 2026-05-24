43d082b963e3f50187e9e649e82d0319b8d1419e
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-24 17:34:16 +0530

    String function- title()

---
+++ str_title.py
@@ -0,0 +1,32 @@
+'''
+String functions- title() : it converts the 1st character of every substring to UPPERCASE.
+- SYNTAX: -
+  var_name = string.title()
+'''
+
+#Example 1: -
+print("The lion king".title())    #1st way
+
+# Output: -
+# The Lion King
+
+a = "the lion king"               #2nd way
+res = a.title()
+print(res)
+
+# Output: -
+# The Lion King
+
+
+#Example 2: -
+print("stuart little".title())    #1st way
+
+# Output: -
+# Stuart Little
+
+b = "stuart little"               #2nd way
+res = b.title()
+print(res)
+
+# Output: -
+# Stuart Little
