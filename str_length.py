5d63fd86b450ec582227656ef8bcbd7d4deba954
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-24 16:44:32 +0530

    String function- len()

---
+++ str_length.py
@@ -0,0 +1,18 @@
+#string functions- len() : it determines length of a string.
+'''
+- SYNTAX: -
+  var_name = len(string)
+'''
+
+#Example 1
+print(len("Congrats"))    #Output: 8
+
+#Example 2
+a = "Congrats"
+result = len(a)
+print(result)    #Output: 8
+
+#Example 3
+a = "Congrats"
+result = len(a)
+print("Length of",a,"=",result)    #Output: Length of Congrats = 8
