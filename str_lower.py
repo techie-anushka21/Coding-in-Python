ce10ae046b4d3f399aa64efa456a3ff7122d9a74
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-24 17:29:44 +0530

    String function- lower()

---
+++ str_lower.py
@@ -0,0 +1,31 @@
+'''
+String functions- lower() : it converts the string to lowercase.
+- SYNTAX: -
+  var_name = literal/variable.lower()
+'''
+
+#Example 1: -
+print("CUTE".lower())          #1st way
+
+# Output: -
+# cute
+
+a = "CUTE"
+res = a.lower()                #2nd way
+print(res)
+
+# Output: -
+# cute
+
+#Example 2: -
+print("Adorable".lower())      #1st way
+
+# Output: -
+# adorable
+
+b = "Adorable"                 #2nd way
+res2 = b.lower()
+print(res2)
+
+# Output: -
+# adorable
