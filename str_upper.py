19e78d7ca31469f20f513fdb7b966dd407dbd582
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-24 17:25:59 +0530

    String function- upper()

---
+++ str_upper.py
@@ -0,0 +1,32 @@
+'''
+String functions- upper() : it converts the string to uppercase.
+- SYNTAX: -
+  var_name = literal/variable.upper()
+'''
+
+#Example 1
+print("cute".upper())     #1st way
+
+# Output: -
+# CUTE
+
+a = "cute"
+res = a.upper()           #2nd way
+print(res)
+
+# Output: -
+# CUTE
+
+
+#Example 2
+print("Delhi".upper())    #1st way
+
+# Output: -
+# DELHI
+
+b = "Delhi"
+res2 = b.upper()          #2nd way
+print(res2)
+
+# Output: -
+# DELHI
