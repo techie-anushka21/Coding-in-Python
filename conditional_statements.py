15f8e677a7537272cc9bf4de212ce91232338d91
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-25 18:51:27 +0530

    Conditional Statements

---
+++ conditional_statements.py
@@ -0,0 +1,41 @@
+'''
+Conditional Statements in Python: -
+- Checks whether the given condition is true or not and executes accordingly.
+- There are 4 Conditional Statements in Python: -
+  
+  1. if Statement
+  - Executes block of code if condition is true.
+  - SYNTAX: -
+    
+    if (condition):
+        statement(s)
+
+
+  2. if...else Statement
+  - Executes one block of code if the condition is true, else executes another block.
+  - SYNTAX: -
+
+    if (condition):
+        statement-1
+    else:
+        statement-2
+  
+  3. elif Statement
+  - Used in case of multiple conditions present.
+  - SYNTAX: -
+
+    if (condition-1):
+        statement-1
+    elif (condition-2):
+        statement-2
+    else:
+        statement-3
+  
+  4. ternary expression 
+  - Short form of if...else statement.
+  - SYNTAX: -
+
+    (Statement-1 if (Condition-1) else Statement-2 if (Condition-2)... else (Statement-n))
+
+Note: Parentheses mentioned around the Conditions are not compulsory.
+'''
