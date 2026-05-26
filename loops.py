3d243ec5f820d916ec75f973ee5cd75b4bf4b3ac
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-26 11:36:18 +0530

    Loops in Python

---
+++ loops.py
@@ -0,0 +1,11 @@
+'''
+Loops in Python: -
+
+- They are used to execute a block of code repeatedly as long as the given condition is true
+  or for a fixed no. of times.
+
+- They are of 2 types: -
+ 
+  1. for loop- Used when no. of iterations are known in advance.
+  2. while loop- Used when no. of iterations are not known in advance.
+'''
