dde479e8add29a0c2cb9e999746b4b6b59c4e96e
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-26 11:45:59 +0530

    while loop

---
+++ while_loop.py
@@ -0,0 +1,27 @@
+#while loop: it iterates over single or multiple statements till the condition is true.
+
+# Example-1: print 1st 10 natural nos.
+num = 1
+while (num<=10):
+    print(num," ",end="")
+    num = num+1
+
+# Output: 1  2  3  4  5  6  7  8  9  10
+
+
+# Example-2: print 1st 10 even nos.
+num = 2
+while(num<=20):
+    print(num," ",end="")
+    num = num+2
+
+# Output: 2  4  6  8  10  12  14  16  18  20
+
+
+# Example-3: print 1st 10 odd nos.
+num = 1
+while (num<=19):
+    print(num," ",end="")
+    num = num+2
+
+# Output: 1  3  5  7  9  11  13  15  17  19
