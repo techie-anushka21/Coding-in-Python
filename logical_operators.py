9a8b781cc6da0864d2a0c6deafab41d83236fc0c
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-26 11:03:15 +0530

    Logical Operators in Python

---
+++ logical_operators.py
@@ -0,0 +1,13 @@
+'''
+Logical operators in Python: -
+
+- They combine/modify Conditional Statements and return boolean result (True or False).
+- They are of 3 types: -
+
+  1. logical 'and' operator- returns True when all conditions are True, else False.
+
+  2. logical 'or' operator- returns True if at least 1 of the conditions is true or both are true. 
+     Returns False only if all conditions are False.
+    
+  3. logical 'not' operator- True->False, False->True
+'''
