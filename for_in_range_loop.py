27f6d9db6391d3a887ec2addcec2309842b829c2
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-26 11:44:46 +0530

    for...in...range loop

---
+++ for_in_range_loop.py
@@ -0,0 +1,126 @@
+#for...in...range LOOP statement- Minimum 1 maximum 3 arguments can be passed.
+
+#Example 1:-
+for a in range(5):
+    print(a)
+
+'''
+Output: -
+
+0
+1
+2
+3
+4
+'''
+
+#Example 2:-
+for i in range(0,5):
+    print(i)
+
+'''
+Output: -
+
+0
+1
+2
+3
+4
+'''
+
+#Example 3:-
+for c in range(1,20,2):
+    print(c)
+
+'''
+Output: -
+
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19
+'''
+
+#Example 4:-
+for v in range(2,20,2):
+    print(v)
+
+'''
+Output: -
+
+2
+4
+6
+8
+10
+12
+14
+16
+18
+'''
+
+#Example 5:-
+for t in range(1,11):
+    print(t)
+
+'''
+Output: -
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+'''
+
+#Example 6:-
+for z in range(5):
+    print("Victory")
+
+'''
+Output: -
+
+Victory
+Victory
+Victory
+Victory
+Victory
+'''
+
+#Example 7:-
+for l in range(0,5):
+   print("Victory")
+
+'''
+Output: -
+
+Victory
+Victory
+Victory
+Victory
+Victory
+'''
+
+#Example 8:-
+for s in range(1,5):
+    print("Victory")
+
+'''
+Output: -
+
+Victory
+Victory
+Victory
+Victory
+'''
