37868ec1c7ffe8d14fdd54a917147841d0a1af7c
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-26 11:41:09 +0530

    for...in loop

---
+++ for_in_loop.py
@@ -0,0 +1,34 @@
+#for...in loop- used to iterate over a collection of items like list, dictionary, sets, tuple, etc
+# or any iterable object.
+
+#Example-1: print the programming languages you have learnt till now.
+print("Here is a list of programming languages I have learnt till now:-")
+for i in("C","Python","C++","Java"):
+    print(i)
+
+'''
+Output: -
+
+Here is a list of programming languages I have learnt till now:-
+C
+Python
+C++
+Java
+'''
+
+
+#Example-2: print your favourite colors.
+print("My favourite colors:-")
+for j in("Pink","Blue","Red","Golden","Silver"):
+    print(j)
+
+'''
+Output: -
+
+My favourite colors:-
+Pink
+Blue
+Red
+Golden
+Silver
+'''
