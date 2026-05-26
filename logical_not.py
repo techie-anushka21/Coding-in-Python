d525ab58a0cc75fa15e39fc0735f5680692dc899
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-26 10:52:29 +0530

    Logical 'not' Operator

---
+++ logical_not.py
@@ -0,0 +1,7 @@
+#logical not operator: True->False, False->True
+
+#Proof:-
+print(not(True))     # Output: False
+print(not(False))    # Output: True
+print(not(5>2))      # Output: False
+print(not(2>5))      # Output: True
