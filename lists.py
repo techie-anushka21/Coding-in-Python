b874319be7baa055afa5e2bab1cdf2a3de0f67c2
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-28 14:32:35 +0530

    Lists in Python

---
+++ lists.py
@@ -0,0 +1,21 @@
+'''
+List:-
+-List is a Sequence data type in Python.
+-It behaves like a Dynamic Array.
+-It can be empty. Empty list can be created by list() or list_name = []
+-It is mutable.
+'''
+
+#Creating our 1st List in Python:-
+my_list = [1,'A','Student',98.6]
+print(my_list)    #Output: [1, 'A', 'Student', 98.6]
+
+#Another list:-
+students = ['Abhi','Surbhi','Heera','Anna']
+print(students)   #Output: ['Abhi', 'Surbhi', 'Heera', 'Anna']
+
+'''In a List of Strings, the names will sort in alphabetical order when we use the sort function.'''
+
+#Empty list
+empty_list = []
+print(empty_list)    #Output: []
