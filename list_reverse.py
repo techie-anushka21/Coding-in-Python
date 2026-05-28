fec6619cb7521e5615e50a225dc0656d9eb2eb8f
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-28 18:07:26 +0530

    7. List function: reverse()

---
+++ list_reverse.py
@@ -0,0 +1,5 @@
+list1 = [1,5,3,3,9,2]    #list creation
+print(list1)             #Output: [1, 5, 3, 3, 9, 2]
+
+list1.reverse()          #command to reverse the list
+print(list1)             #Output: [2, 9, 3, 3, 5, 1]
