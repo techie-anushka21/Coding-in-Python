e952e3effe9f5259562c941499563756a294d520
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-28 13:48:28 +0530

    3. List function: index()

---
+++ list_index.py
@@ -0,0 +1,12 @@
+#Example 1:- List of Integers
+list4 = [10,20,30,40,50,60,50]    #list creation
+print(list4)                   #Output: [10, 20, 30, 40, 50, 60, 50]
+
+#Accessing the index of number 50 in the list
+print(list4.index(50))         #Output: 4 (element 50 is on the 4th index, 1st occurence)
+
+#Example 2:- List of Strings
+list5 = ['Ruby','Sapphire','Emerald','Topaz']    #list creation
+
+#Accessing the index of string 'Ruby' in the list
+print(list5.index('Ruby'))     #Output: 0 (element 'Ruby' is on the 0th index)
