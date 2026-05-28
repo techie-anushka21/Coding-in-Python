ea7cba14ae650c5bf0f21bfe1f1bb97d93077033
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-28 18:22:45 +0530

    8. List function: count()

---
+++ list_count.py
@@ -0,0 +1,5 @@
+list1 = [10,20,30,40,50,10,40,20,60]    #list creation
+print(list1)                            #Output: [10, 20, 30, 40, 50, 10, 40, 20, 60]
+
+print(list1.count(10))    #count the no. of 10s occurring in list1, Output: 2 (10 is occurring 2 times in the list)
+print(list1.count(60))    #count the no. of 60s occurring in list1, Output: 1 (60 is occurring 1 time in the list)
