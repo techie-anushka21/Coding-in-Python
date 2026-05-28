c9bba716aa5796ff5cd152c3f1cdf168c9d6f2fc
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-28 14:14:20 +0530

    5. List function: pop()

---
+++ list_pop.py
@@ -0,0 +1,5 @@
+list1 = [10,20,30,40,50]    #list creation
+print(list1)                #Output: [10, 20, 30, 40, 50]
+
+list1.pop(2)                #command to delete element at index 2 of the list
+print(list1)                #Output: [10, 20, 40, 50]
