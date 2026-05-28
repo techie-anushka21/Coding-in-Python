f13fa6675593852b76bd50fe5f8a00f1accdf43b
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-28 12:52:29 +0530

    2. List function: extend()

---
+++ list_extend.py
@@ -0,0 +1,8 @@
+list1 = [1,2,3,4]    #Creating a list
+print(list1)         #Output: [1, 2, 3, 4]
+
+list2 = [5,6,7]      #Creating another list
+print(list2)         #Output: [5, 6, 7]
+
+list1.extend(list2)  #Extending list1
+print(list1)         #Output: [1, 2, 3, 4, 5, 6, 7]
