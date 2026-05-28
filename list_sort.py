132441d04c7b23c32de708a1804bc3c519824511
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-28 18:47:08 +0530

    9. List function: sort()

---
+++ list_sort.py
@@ -0,0 +1,8 @@
+list1 = [15,31,1,9]    #list creation
+print(list1)           #Output: [15, 31, 1, 9]
+
+list1.sort()           #command to sort the list1 elements in ASCENDING order
+print(list1)           #Output: [1, 9, 15, 31]
+
+list1.sort(reverse=True)    #command to sort the list1 elements in DESCENDING order
+print(list1)                #Output: [31, 15, 9, 1]
