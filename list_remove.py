bf2962774f38e523ec4533e9affbe6e76f0ca138
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-28 18:04:58 +0530

    6. List function: remove()

---
+++ list_remove.py
@@ -0,0 +1,5 @@
+list1 = [10,20,30,40,50,60,50]    #list creation
+print(list1)                #Output: [10, 20, 30, 40, 50, 60, 50]
+
+list1.remove(50)            #command to remove 50 from the list
+print(list1)                #Output: [10, 20, 30, 40, 60, 50]
