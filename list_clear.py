c4dcf9ceb44c76a2abb559dfd2e9e682bf3baeb1
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-29 12:04:50 +0530

    10. List function: clear()

---
+++ list_clear.py
@@ -0,0 +1,5 @@
+the_list = [10,20,30,40]    #list creation
+print(the_list)             #Output: [10, 20, 30, 40]
+
+the_list.clear()            #Applying the clear() method to clear the_list
+print(the_list)             #Output: []
