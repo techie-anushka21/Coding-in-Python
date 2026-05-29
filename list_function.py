afa3227883dc3b009dc7ebd9b4054612e1b64ada
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-29 12:24:01 +0530

    12. List function: list()

---
+++ list_function.py
@@ -0,0 +1,14 @@
+'''
+ Using list() function in Python
+-It is another way of creating lists in Python.
+'''
+
+#Empty list:-
+empty_list2 = list()
+print(empty_list2)          # Output: []
+print(type(empty_list2))    # Output: <class 'list'>
+
+#Non-empty list:-
+emp2 = list([102,'Harshie',44.8,'HoD-IT'])
+print(emp2)                 #Output: [102, 'Harshie', 44.8, 'HoD-IT']
+print(type(emp2))    # Output: <class 'list'>
