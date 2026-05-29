d724484b3f407a378ce46b91f14d009c0335027e
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-29 12:13:18 +0530

    11. List function: copy()

---
+++ list_copy.py
@@ -0,0 +1,5 @@
+list1 = [10,20,30,40,50]    #list creation
+print(list1)                #Output: [10, 20, 30, 40, 50]
+
+list2 = list1.copy()        #command to copy list 1 elements to list 2
+print(list2)                #Output: [10, 20, 30, 40, 50]
