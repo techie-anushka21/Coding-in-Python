d1862c09087af78ce7af2cfac9001e7bece9cc1d
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-29 13:31:24 +0530

    2. Tuple function: count()

---
+++ tuple_count.py
@@ -0,0 +1,5 @@
+tuple1 = (10,20,30,10,40,20,10,50,60,10)    #tuple creation
+print(tuple1)
+
+print(tuple1.count(10))    #counts and prints the number of 10s occuring in the tuple, tuple1, Ouptut: 4
+print(tuple1.count(20))    #counts and prints the number of 20s occuring in the tuple, tuple1, Ouptut: 2
