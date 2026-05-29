11a97b224bb6eca6c3b82565f7edeb7859cdd97f
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-29 13:48:15 +0530

    Tuple Methods

---
+++ tuple_methods.py
@@ -0,0 +1,21 @@
+'''
+Below are the Tuple Methods we use in Python:-
+
+1. tuple()
+- Another way of creating a tuple.
+- It can contain only 1 iterable.
+- SYNTAX: tuple(iterable)
+
+2. count()
+- It counts the number of times the specified element occurs in a Tuple.
+- Output will be 0 if the mentioned element does not belong to the Tuple.
+- SYNTAX: tuple_name.count(element)
+
+3. index()
+- It returns the index of 1st occurrence of the given element.
+- SYNTAX: tuple_name.index(element,start,end)
+- Here start is the index from which searching starts.
+- Here end index is not included in the search (end-1 is where the search ends).
+- Raises ValueError if mentioned element is not present in the tuple.
+
+'''
