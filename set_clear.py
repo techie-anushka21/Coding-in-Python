e386ba1dd383c30c77d0b34a75bd358dbef52286
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-30 19:53:26 +0530

    2. Set function: clear()

---
+++ set_clear.py
@@ -0,0 +1,11 @@
+#Example:-
+marks_set = {98,95,99,87,91}
+print(marks_set)    #Output: {98, 99, 87, 91, 95}
+
+marks_set.clear()   #Command to clear the set
+print(marks_set)    #Output: set()
+
+'''
+Note: The output mentioned in 1st Example is a possible output, not guaranteed (you might get a different 
+      order of output).
+'''
