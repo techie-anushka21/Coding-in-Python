f7c2c5e9fc66d2ae75d650787e23d62a89754203
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-30 21:21:41 +0530

    7. Set function: set()

---
+++ set_function.py
@@ -0,0 +1,19 @@
+#Empty set:-
+empty_set = set()
+print(empty_set)    #Output: set()
+print(type(empty_set))    #Output: <class 'set'>
+
+#Another example:-
+set3 = set("HAPPY")
+print(set3)         #Output: {'Y', 'A', 'P', 'H'}
+
+#Example-3:-
+set4 = set({"Pink","Orange","Green","Golden"})
+print(set4)         #Output: {'Pink', 'Orange', 'Golden', 'Green'}
+print(len(set4))    #Output: 4 (means there are 4 elements in the set)
+print(type(set4))   #Output: <class 'set'>
+
+#Example-4: -
+set5 = set(["HAPPY"])
+print(set5)          #Output: {'HAPPY'}
+print(type(set5))    #Output: <class 'set'>
