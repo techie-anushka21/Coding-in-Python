1c9755c2742eaf9a5e190721266609bf22f94530
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-30 21:45:48 +0530

    List Methods and list() Constructor function

--- list_methods.py
+++ list_methods.py
@@ -1,59 +1,64 @@
 '''
 Below are the List Methods we use in Python:-
 
-1. list()
--It is another way of creating lists in Python.
-- SYNTAX:
-  1. For empty list: list_name = list()
-  2. For non-empty list: list_name = list([element-1, element-2,...., element-n])  
-
-2. append()
+1. append()
 - Adds a new element to the existing list.
 - If another list is added, then this leads to a nested list.
 - SYNTAX: list_name.append(element)
 
-3. extend()
+2. extend()
 - Expands the existing list.
 - All elements are separate. 
 - SYNTAX: list_name.extend(iterable)
 
-4. index()
+3. index()
 - It returns the starting index of the element mentioned in the parentheses.
 - Raises ValueError if the element is not in the list.
 - SYNTAX: list_name.index(element)
 
-5. insert()
+4. insert()
 - It inserts the given element into an existing List.
 - SYNTAX: list_name.insert(index_no,element)
 
-6. pop()
+5. pop()
 - It removes and returns the element at the specified index.
 - If no element is given then it removes the last value.
 - SYNTAX: list_name.pop(index_of_the_element)  
 
-7. remove()
+6. remove()
 - It removes the 1st occurrence of the specified element from the list.
 - SYNTAX: list_name.remove(element) 
 
-8. reverse()
+7. reverse()
 - It reverses the order of elements in a list.
 - SYNTAX: list_name.reverse()
 
-9. count()
+8. count()
 - Counts the occurrence of an element in a list.
 - SYNTAX: list_name.count(element)
 
-10. sort()
+9. sort()
 - Sorts the given list in ascending or descending order.
 - SYNTAX:
   1. For sorting in Ascending order: list_name.sort()
   2. For sorting in Descending order: list_name.sort(reverse=True)
 
-11. clear()
+10. clear()
 - It removes all the elements from the list.
 - SYNTAX: list_name.clear()
 
-12. copy()
+11. copy()
 - It creates a shallow copy of the list.
 - SYNTAX: list_name.copy() 
+'''
+
+'''
+Also: -
+
+1. list()
+- It is a Constructor function.
+- It is another way of creating lists in Python.
+- SYNTAX:
+  1. For empty list: list_name = list()
+  2. For non-empty list: list_name = list([element-1, element-2,...., element-n])
 '''
