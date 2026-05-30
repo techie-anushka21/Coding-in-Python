7ae2c204ba01ebfb2030595ba4ec0b46c682cd8c
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-30 21:31:20 +0530

    Set and Tuple

---
+++ set_and_tuple.py
@@ -0,0 +1,19 @@
+'''
+Set and Tuple:-
+'''
+
+#Example-1:-
+set_and_tuple = set(("Pink","Green","Golden","Blue"))
+print(set_and_tuple)          #Output: {'Green', 'Blue', 'Pink', 'Golden'}
+print(type(set_and_tuple))    #Output: <class 'set'>
+
+
+#Example-2:-
+set_and_tuple2 = set((1.1,2.2,3.3,4.4,5.5,6.6))
+print(set_and_tuple2)         #Output: {1.1, 2.2, 3.3, 4.4, 5.5, 6.6}
+print(type(set_and_tuple2))    #Output: <class 'set'>
+
+'''
+Note: The outputs mentioned are possible outputs, not guaranteed (you might get a different 
+      order of output).
+'''
