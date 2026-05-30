7575f875dbf128ce13e5680bbb6fa00522265b9c
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-30 21:25:04 +0530

    Set and List

---
+++ set_and_list.py
@@ -0,0 +1,19 @@
+'''
+Set and List:-
+'''
+
+#Example-1:-
+set_and_list = set([10,10,20,30,40,50])
+print(set_and_list)    #Output: {40, 10, 50, 20, 30}
+print(type(set_and_list))    #<class 'set'>
+
+
+#Example-2:-
+set_and_list2 = set(["pink","green","yellow","golden"])
+print(set_and_list2)   #Output: {'golden', 'yellow', 'pink', 'green'}
+print(type(set_and_list2))    #<class 'set'>
+
+'''
+Note: The outputs mentioned are possible outputs, not guaranteed (you might get a different 
+      order of output).
+'''
