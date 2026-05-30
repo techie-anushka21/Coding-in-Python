53b8dfdd824edf79a6e5440f1a5f34804ccf8b8f
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-30 19:49:22 +0530

    1. Set function: add()

---
+++ set_add.py
@@ -0,0 +1,18 @@
+#Example-1:-
+colors = {"pink","red","green","blue"}
+print(colors)    #Output: {'green', 'pink', 'blue', 'red'}
+
+colors.add("golden")
+print(colors)    #Output: {'blue', 'green', 'golden', 'red', 'pink'}
+
+#Example-2:-
+marks = {90,96,92,93}
+print(marks)     #Output: {96, 90, 92, 93}
+
+marks.add(95)
+print(marks)     #Output: {96, 90, 92, 93, 95}
+
+'''
+Note: The outputs mentioned are possible outputs, not guaranteed (you might get a different 
+      order of output).
+'''
