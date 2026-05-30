7d1c61b4f059c6aaea2a2469f873b3d3c73b763e
Anushka Singh <ms.anushka2108@gmail.com>
2026-05-30 20:40:41 +0530

    3. Set function: pop()

---
+++ set_pop.py
@@ -0,0 +1,13 @@
+#Example:-
+marks_set = {98,95,99,87,91}
+print(marks_set)    #Output: {98, 99, 87, 91, 95}
+
+c = marks_set.pop()
+print(c)            #Output: 98
+
+print(marks_set)    #Output: {99, 87, 91, 95}
+
+'''
+Note: The outputs mentioned are possible outputs, not guaranteed (you might get a different 
+      order of output).
+'''
