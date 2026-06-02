9f54eeca94d85f4783b926ed6e9dea2401840ad4
Anushka Singh <ms.anushka2108@gmail.com>
2026-06-02 23:40:44 +0530

    Dictionary Keys Example

---
+++ dict_keys_example1.py
@@ -0,0 +1,21 @@
+'''
+Example: -
+Iterating over keys: -
+'''
+
+students = {1: "Abhi", 2: 'Dipti', 3: 'Divyanka', 4: 'Esha', 5: 'Harry'}
+count = 0
+for keys in students:
+    print(keys)
+    count = count+1
+print("No. of keys in the dictionary =",count)
+
+'''
+Output:
+1
+2
+3
+4
+5
+No. of keys in the dictionary = 5
+'''
