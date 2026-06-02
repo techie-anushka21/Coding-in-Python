a1bd807640f92bf2b377f72e5b652ae917814c21
Anushka Singh <ms.anushka2108@gmail.com>
2026-06-02 21:27:53 +0530

    3. Dictionary method: pop()

---
+++ dict_pop.py
@@ -0,0 +1,9 @@
+#Example:-
+students = {1:"Abhi", 2:"Vidhi", 3:"Khyati", 4:"Vineet"}
+print(students)    #Output: {1:"Abhi", 2:"Vidhi", 3:"Khyati", 4:"Vineet"}
+
+#To pop key 3 and return its value:-
+value = students.pop(3)
+print(value)       #Output: Khyati
+
+print(students)    #Output: {1:"Abhi", 2:"Vidhi", 4:"Vineet"}
