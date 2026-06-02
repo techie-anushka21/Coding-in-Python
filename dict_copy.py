7eb7ce5f22a8cac37ef2a92236e8bebc70416fa2
Anushka Singh <ms.anushka2108@gmail.com>
2026-06-02 22:45:04 +0530

    6. Dictionary method: copy()

---
+++ dict_copy.py
@@ -0,0 +1,9 @@
+#Example: -
+
+dict1 = {1: "Abhi", 2: 'Dipti', 3: 'Divyanka', 4: 'Esha', 5: 'Harry'}
+print(dict1)
+#Output: {1: "Abhi", 2: 'Dipti', 3: 'Divyanka', 4: 'Esha', 5: 'Harry'}
+
+dict2 = dict1.copy()
+print(dict2)
+#Output: {1: "Abhi", 2: 'Dipti', 3: 'Divyanka', 4: 'Esha', 5: 'Harry'}
