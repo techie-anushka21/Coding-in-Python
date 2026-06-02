b8f40f1821aa20f35bc18160bee20d5bda4cf202
Anushka Singh <ms.anushka2108@gmail.com>
2026-06-02 22:41:47 +0530

    5. Dictionary method: clear()

---
+++ dict_clear.py
@@ -0,0 +1,8 @@
+#Example: -
+
+students = {1: "Abhi", 2: 'Dipti', 3: 'Divyanka', 4: 'Esha', 5: 'Harry'}
+print(students)
+#Output: {1: "Abhi", 2: 'Dipti', 3: 'Divyanka', 4: 'Esha', 5: 'Harry'}
+
+students.clear()
+print(students)    #Output: {}
