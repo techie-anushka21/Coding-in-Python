707925ccff5b88bbd72d53476ea72b603521e83e
Anushka Singh <ms.anushka2108@gmail.com>
2026-06-02 23:10:38 +0530

    dict() : Built-in Constructor function

---
+++ dict_constructor.py
@@ -0,0 +1,9 @@
+#Example:-
+student_streams = dict(Abhi ='PCM', Surbhi = 'PCB', Divyanka = 'Commerce', Arjun = 'Humanities')
+print(student_streams)
+#Output-> {'Abhi': 'PCM', 'Surbhi': 'PCB', 'Divyanka': 'Commerce', 'Arjun': 'Humanities'}
+
+
+#Empty Dictionary:-
+dict2 = dict()
+print(dict2)    #Output: {}
