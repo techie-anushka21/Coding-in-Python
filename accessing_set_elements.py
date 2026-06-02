411a8788ba9d7a307a88ce10be42406bcdbc8f4a
Anushka Singh <ms.anushka2108@gmail.com>
2026-06-02 20:38:23 +0530

    Accessing Set Elements

---
+++ accessing_set_elements.py
@@ -0,0 +1,31 @@
+'''
+Accessing set elements:-
+'''
+
+#Example-1:-
+my_set = {"Pink","Green","Golden","Blue"}
+for color in my_set:
+    print(color)
+
+'''
+Output:-
+
+Pink
+Blue
+Golden
+Green
+'''
+
+#Example-2:-
+my_set2 = {97.5,94,91,95}
+for marks in my_set2:
+    print(marks)
+
+'''
+Output:-
+
+97.5
+91
+94
+95
+'''
