8a10b7a398123e746fd5320cd5ea0df3b32ac5b0
Anushka Singh <ms.anushka2108@gmail.com>
2026-06-02 21:17:51 +0530

    2. Dictionary method: items()

---
+++ dict_items.py
@@ -0,0 +1,24 @@
+#Example 1:-
+students = {1: "Abhi", 2: 'Dipti', 3: 'Divyanka', 4: 'Esha', 5: 'Harry'}
+for key,value in students.items():
+    print(key,value)
+
+'''Output:  1 Abhi
+            2 Dipti
+            3 Divyanka
+            4 Esha
+            5 Harry
+'''
+
+#Example 2:-
+emp = {101:'Abhi', 102:'Dipti', 103:'Divyanka', 104: 'Esha', 105: 'Harry'}
+for key,value in emp.items():
+    print(key,"=",value)
+
+'''
+Output: 101 = Abhi
+        102 = Dipti
+        103 = Divyanka
+        104 = Esha
+        105 = Harry
+'''
