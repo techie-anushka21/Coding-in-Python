9fcd144f6bf9fa7adf493b0b892f43c8bce9ed02
Anushka Singh <ms.anushka2108@gmail.com>
2026-06-02 20:57:18 +0530

    Adding new element to an Existing Dictionary

---
+++ dict_add_new_element.py
@@ -0,0 +1,14 @@
+'''
+Adding new element in an Existing dictionary:-
+SYNTAX: dictionary_name[key] = value/"value"
+'''
+
+#Example:-
+students = {1:"Abhi", 2:"Cutie", 3: "Divyanka", 4: "Esha"}
+print(students)
+#Output: {1: 'Abhi', 2: 'Cutie', 3: 'Divyanka', 4: 'Esha'}
+
+#Adding 5:"Harry" to the above dictioanry:-
+students[5]="Harry"
+print(students)
+#Output: {1: 'Abhi', 2: 'Cutie', 3: 'Divyanka', 4: 'Esha', 5: 'Harry'}
